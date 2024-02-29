a2a48b6cede04bc940f20f87a15269a8a5b046a1
ThanhDat19 <toidabotay@gmail.com>
2024-02-29 07:57:44 +0700

    add template engine

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express"); //Imports the express module
+const app = express(); //Creates an instance of the express module
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//Use template engine
+app.set("view engine", "pug");
+
+//Creates a Root Route
+app.get("/", (req, res) => {
+  res.render("index", { title: "Hey", message: "Hello there!" });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  // Thay đổi nội dung trả về để truy cập trang web nhanh hơn
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
